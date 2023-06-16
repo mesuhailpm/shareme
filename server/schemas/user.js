b7532f3ebb27a29885d310b9517d8f13e40a8844
suhail <tosuhailpm@gmail.com>
2023-06-17 00:22:15 +0530

    initial commit

---
+++ server/schemas/user.js
@@ -0,0 +1,18 @@
+export default {
+    name:'user',
+    title:'User',
+    type:'document',
+    fields:[
+        {
+            name:'name',
+            type:'string',
+            title:'Name'
+
+        },
+        {
+            name:'Image',
+            type:'string',
+            title:'Image'
+        }
+    ]
+}
