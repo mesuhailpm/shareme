8991ca5716374f4c84c2b39395e13a4c9730485a
suhail <tosuhailpm@gmail.com>
2023-06-17 14:08:52 +0530

    commiting

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes, useNavigate} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <div className="App">App compoente</div>
+            <Routes>
+                
+                <Route path='/login' element={<Login />}>Login</Route>
+                <Route path='/' element={<Home/>}>Home</Route>
+            </Routes>
+        </>
         )
     }
     
