a9cee9e78209f39cc3714f74525f3f8e584974f9
suhail <tosuhailpm@gmail.com>
2023-06-27 08:42:06 +0530

    implemented search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,8 +1,42 @@
-const Search = () => {
+import React, {useState, useEffect} from 'react';
+import { searchQuery } from '../utils/generateQuery';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+
+
+const Search = ({searchTerm}) => {
+    const [pins,setPins] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        if(searchTerm !== ''){
+            setIsLoading(true)
+            const query  = searchQuery(searchTerm.toLowerCase())
+            client.fetch(query)
+                .then((response) => {
+                    setIsLoading(false)
+                    setPins(response)})
+                .catch(err=>{
+                    setIsLoading(false)
+                    console.log(err)})
+        }
+
+    },
+    [searchTerm])
+    
     return(
-        <>
-            Search
-        </>
+        <div>
+            {isLoading ? <Spinner message={`searching with '${searchTerm}'`}/>:
+            <MasonryLayout pins = {pins}/>
+            }
+            {(searchTerm && !isLoading) && (
+                <p> No pins matches your search</p>
+
+            )}
+        </div>
+        
     )
 }
 
